ad36a83b55e633cb0c165804a38aab2384e1bf13
Krsatvik1 <krsatvik@gmail.com>
2024-10-21 15:56:39 +0530

    refine v4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
   import Web from "$lib/components/icons/web.svelte";
   import WebCode from "$lib/components/icons/webCode.svelte";
   import ai from "$lib/components/icons/ai.svelte";
+  import learn from "$lib/components/icons/learn.svelte";
   import { borderAll } from "$lib/commonClasses.js";
 </script>
 
@@ -92,7 +93,7 @@
         <div
           class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
         >
-          2024
+          2023
         </div>
         <div
           class=" text-stone-900 pt-[0.7dvi] text-h1_sm lg:text-h1_lg font-normal font-['ringold-sans']"
@@ -120,41 +121,283 @@
 </a>
 
 <Title label="Web Development" text_size="h1" height="h-[45dvi] lg:h-[15dvi]" overide="mt-[30dvi] lg:mt-[10dvi] lg:col-span-4 col-span-4"/>
-<WorkItem
-  label="Britinfotech"
-  tags="Project Management / Web Development / Live"
-  link="https://britinfotech.com/"
-  img="britinfotech"
-/>
-<WorkItem
-  label="Portfolio for Massive Works"
-  tags="Web Development / CMS"
-  link="https://massiveworks.krsatvik.xyz/"
-  img="massiveworks"
-/>
-<WorkItem
-  label="Portfolio for Parth"
-  tags="Web Development / CMS / Live"
-  link="https://parthsuratwala.com/"
-  img="parth"
-/>
-<Title label="Visual Communication" text_size="h1" height="h-[45dvi] lg:h-[15dvi]" overide="mt-[30dvi] lg:mt-[10dvi] lg:col-span-4 col-span-4"/>
-<WorkItem
-  label="Creative Coding - Designer's Guide"
-  tags="Publication / Creative Coding / Classroom Assignment"
-  link="https://www.behance.net/gallery/193824279/Creative-Coding-Designers-Guider"
-  img="creativecoding"
-/>
-<WorkItem
-  label="Zine: Art Deco Movement"
-  tags="Art History / Publication / Classroom Assignment"
-  link="https://www.behance.net/gallery/207261611/Zine-Art-Deco-Movement"
-  img="zine"
-/>
-<WorkItem
-  label="Ad Concept for NOTO"
-  tags="Market Research / Ad Design / Classroom Assignment"
-  link="https://www.behance.net/gallery/170368281/Ad-Concept-for-NOTO"
-  img="adconcept"
-/>
+<a
+  href="https://britinfotech.com/"
+  class="col-span-4 mt-[15dvi] lg:mt-[5dvi] lg:grid-cols-2 grid group "
+>
+  <div class="{borderAll} lg:h-[25dvi] h-[75dvi] bg-white">
+    <div class="h-full w-full overflow-hidden">
+      <enhanced:img
+      class="object-cover object-left-top origin-top-left mx-auto group-hover:scale-[1.1] transition-all duration-200 h-full"
+      src="$lib/images/britinfotech/cover.png"
+      alt="Type System"
+    />
+    </div>
+    
+  </div>
+  <div>
+    <div
+      class="{borderAll} relative block bg-opacity-70 h-[60dvi] lg:h-[20dvi] bg-white"
+    >
+      <div
+        class="px-[5dvi] lg:px-[2dvi] max-w-[80dvi] flex-col justify-center h-full items-start gap-[4dvi] lg:gap-[1.5dvi] inline-flex"
+      >
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+          2023
+        </div>
+        <div
+          class=" text-stone-900 pt-[0.7dvi] text-h1_sm lg:text-h1_lg font-normal font-['ringold-sans']"
+        >
+        Britinfotech
+        </div>
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+        Project Management / Web Development / Live
+        </div>
+      </div>
+    </div>
+    <div class="flex w-fit">
+      <IconBox>
+        <svelte:component this={manager} />
+      </IconBox>
+      <IconBox>
+        <svelte:component this={code} />
+      </IconBox>
+    </div>
+  </div>
+</a>
+<a
+  href="https://massiveworks.krsatvik.xyz/"
+  class="col-span-4 mt-[15dvi] lg:mt-[5dvi] lg:grid-cols-2 grid group"
+>
+  <div class="order-2 lg:order-1">
+    <div class="flex w-full justify-end">
+      <IconBox>
+        <svelte:component this={code} />
+      </IconBox>
+    </div>
+    <div
+      class="{borderAll} relative block bg-opacity-70 h-[60dvi] lg:h-[25dvi] bg-white "
+    >
+      <div
+        class="px-[5dvi] lg:px-[2dvi] max-w-[80dvi] flex-col justify-center h-full items-start gap-[4dvi] lg:gap-[1.5dvi] inline-flex"
+      >
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+          2023
+        </div>
+        <div
+          class=" text-stone-900 pt-[0.7dvi] text-h1_sm lg:text-h1_lg font-normal font-['ringold-sans']"
+        >
+        Portfolio for Massive Works
+        </div>
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+        Web Development / CMS
+        </div>
+      </div>
+    </div>
+    
+  </div>
+  <div class="{borderAll} lg:h-[30dvi] h-[75dvi] order-2 lg:order-2">
+    <div class="h-full w-full overflow-hidden">
+      <enhanced:img
+      class="object-cover origin-top-left mx-auto group-hover:scale-[1.1] transition-all duration-200 h-full"
+      src="$lib/images/massiveworks/cover.png"
+      alt="Type System"
+    />
+    </div>
+  </div>
+</a>
+<a
+  href="https://parthsuratwala.com/"
+  class="col-span-4 mt-[15dvi] lg:mt-[5dvi] lg:grid-cols-2 grid group "
+>
+  <div class="{borderAll} lg:h-[25dvi] h-[75dvi] bg-white">
+    <div class="h-full w-full overflow-hidden">
+      <enhanced:img
+      class="object-cover object-left-top origin-top-left mx-auto group-hover:scale-[1.1] transition-all duration-200 h-full"
+      src="$lib/images/parth/cover.png"
+      alt="Type System"
+    />
+    </div>
+    
+  </div>
+  <div>
+    <div
+      class="{borderAll} relative block bg-opacity-70 h-[60dvi] lg:h-[20dvi] bg-white"
+    >
+      <div
+        class="px-[5dvi] lg:px-[2dvi] max-w-[80dvi] flex-col justify-center h-full items-start gap-[4dvi] lg:gap-[1.5dvi] inline-flex"
+      >
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+          2023
+        </div>
+        <div
+          class=" text-stone-900 pt-[0.7dvi] text-h1_sm lg:text-h1_lg font-normal font-['ringold-sans']"
+        >
+        Portfolio for Parth
+        </div>
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+        Web Development / CMS / Live
+        </div>
+      </div>
+    </div>
+    <div class="flex w-fit">
+      <IconBox>
+        <svelte:component this={code} />
+      </IconBox>
+    </div>
+  </div>
+</a>
+
+<Title label="Visual Design" text_size="h1" height="h-[45dvi] lg:h-[15dvi]" overide="mt-[30dvi] lg:mt-[10dvi] lg:col-span-4 col-span-4"/>
+<a
+  href="https://www.behance.net/gallery/193824279/Creative-Coding-Designers-Guider"
+  class="col-span-4 mt-[15dvi] lg:mt-[5dvi] lg:grid-cols-2 grid group "
+>
+  <div class="{borderAll} lg:h-[40dvi] h-[75dvi] bg-white">
+    <div class="h-full w-full overflow-hidden">
+      <enhanced:img
+      class="object-cover origin-top-left mx-auto group-hover:scale-[1.1] transition-all duration-200 h-full"
+      src="$lib/images/creativecoding/cover.gif"
+      alt="Type System"
+    />
+    </div>
+    
+  </div>
+  <div>
+    <div
+      class="{borderAll} relative block bg-opacity-70 h-[75dvi] lg:h-[35dvi] bg-white"
+    >
+      <div
+        class="px-[5dvi] lg:px-[2dvi] max-w-[80dvi] flex-col justify-center h-full items-start gap-[4dvi] lg:gap-[1.5dvi] inline-flex"
+      >
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+          2024
+        </div>
+        <div
+          class=" text-stone-900 pt-[0.7dvi] text-h1_sm lg:text-h1_lg font-normal font-['ringold-sans']"
+        >
+        Creative Coding - Designer's Guide
+        </div>
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+        Publication / Creative Coding / Classroom Assignment
+        </div>
+      </div>
+    </div>
+    <div class="flex w-fit">
+      <IconBox>
+        <svelte:component this={brush} />
+      </IconBox>
+    </div>
+  </div>
+</a>
+<a
+  href="https://www.behance.net/gallery/207261611/Zine-Art-Deco-Movement"
+  class="col-span-4 mt-[15dvi] lg:mt-[5dvi] lg:grid-cols-2 grid group"
+>
+  <div class="order-2 lg:order-1">
+    <div class="flex w-full justify-end">
+      <IconBox>
+        <svelte:component this={brush} />
+      </IconBox>
+      <IconBox>
+        <svelte:component this={learn} />
+      </IconBox>
+    </div>
+    <div
+      class="{borderAll} relative block bg-opacity-70 h-[60dvi] lg:h-[25dvi] bg-white "
+    >
+      <div
+        class="px-[5dvi] lg:px-[2dvi] max-w-[80dvi] flex-col justify-center h-full items-start gap-[4dvi] lg:gap-[1.5dvi] inline-flex"
+      >
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+          2022
+        </div>
+        <div
+          class=" text-stone-900 pt-[0.7dvi] text-h1_sm lg:text-h1_lg font-normal font-['ringold-sans']"
+        >
+        Zine: Art Deco Movement
+        </div>
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+        Art History / Publication / Classroom Assignment
+        </div>
+      </div>
+    </div>
+    
+  </div>
+  <div class="{borderAll} lg:h-[30dvi] h-[75dvi] order-2 lg:order-2">
+    <div class="h-full w-full overflow-hidden">
+      <enhanced:img
+      class="object-cover mx-auto group-hover:scale-[1.1] transition-all duration-200 h-full"
+      src="$lib/images/zine/cover.png"
+      alt="Type System"
+    />
+    </div>
+  </div>
+</a>
+<a
+  href="https://www.behance.net/gallery/193824279/Creative-Coding-Designers-Guider"
+  class="col-span-4 mt-[15dvi] lg:mt-[5dvi] lg:grid-cols-2 grid group "
+>
+  <div class="{borderAll} lg:h-[40dvi] h-[75dvi] bg-white">
+    <div class="h-full w-full overflow-hidden">
+      <enhanced:img
+      class="object-cover origin-top-left mx-auto group-hover:scale-[1.1] transition-all duration-200 h-full"
+      src="$lib/images/adconcept/cover.jpg"
+      alt="Type System"
+    />
+    </div>
+    
+  </div>
+  <div>
+    <div
+      class="{borderAll} relative block bg-opacity-70 h-[60dvi] lg:h-[35dvi] bg-white"
+    >
+      <div
+        class="px-[5dvi] lg:px-[2dvi] max-w-[80dvi] flex-col justify-center h-full items-start gap-[4dvi] lg:gap-[1.5dvi] inline-flex"
+      >
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+          2023
+        </div>
+        <div
+          class=" text-stone-900 pt-[0.7dvi] text-h1_sm lg:text-h1_lg font-normal font-['ringold-sans']"
+        >
+        Ad Concept for NOTO
+        </div>
+        <div
+          class=" text-stone-900 text-cap_sm lg:text-cap_lg font-bold uppercase"
+        >
+        Market Research / Ad Design / Classroom Assignment
+        </div>
+      </div>
+    </div>
+    <div class="flex w-fit">
+      <IconBox>
+        <svelte:component this={brush} />
+      </IconBox>
+    </div>
+  </div>
+</a>
+
 
